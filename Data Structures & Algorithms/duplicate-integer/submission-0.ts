57b1bd8222d6f3851613399aa5b83f05e62f72ba
Atirson Fabiano <fabiano.oliveira2213@gmail.com>
2026-07-02 10:12:14 -0300

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.ts
@@ -0,0 +1,18 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {boolean}
+     */
+    hasDuplicate(nums: number[]): boolean {
+        const map = new Set()
+        for (const num of nums) {
+            if (map.has(num)) {
+                return true
+            }
+
+            map.add(num)
+        }
+
+        return false
+    }
+}
